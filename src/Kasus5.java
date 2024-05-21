f388d1e939a4e32792e306a0f5253ee76e6667b1
Muhammad Affandes <affandes@uin-suska.ac.id>
2024-05-21 10:52:50 +0700

    Update Kasus5

--- src/Kasus5.java
+++ src/Kasus5.java
@@ -1,8 +1,37 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Kasus5 {
     public static void main(String[] args) {
 
         // Load semua nilai dan masukkan ke dalam Class PersegiPanjang
+        String namaFile = "src/sisi.txt";
+        List<PersegiPanjang> persegiPanjang = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(namaFile))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(" ");
+                PersegiPanjang p = new PersegiPanjang(data[0], data[1]);
+                persegiPanjang.add(p);
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+        System.out.println(persegiPanjang);
 
+        // Cari PersegiPanjang yang Luasnya paling besar
+        int index = 0;
+        for (int i = 0; i < persegiPanjang.size(); i++) {
+            if (persegiPanjang.get(i).luas() < persegiPanjang.get(index).luas()) {
+                index = i;
+            }
+        }
+
+        System.out.println("Luas paling luas " + persegiPanjang.get(index).luas());
+        System.out.println("index " + index);
 
     }
 }
--- src/PersegiPanjang.java
+++ src/PersegiPanjang.java
@@ -3,6 +3,14 @@ public class PersegiPanjang {
     public int panjang;
     public int lebar;
 
+    PersegiPanjang() {
+    }
+
+    PersegiPanjang(String panjang, String lebar) {
+        this.panjang = Integer.parseInt(panjang);
+        this.lebar = Integer.parseInt(lebar);
+    }
+
     PersegiPanjang(int panjang, int lebar) {
         this.panjang = panjang;
         this.lebar = lebar;
@@ -20,4 +28,8 @@ public class PersegiPanjang {
         System.out.println(luas() + satuan);
     }
 
+    @Override
+    public String toString() {
+        return "Luas PersegiPanjang: " + this.luas();
+    }
 }
